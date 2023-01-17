ebca9c9a341920d5f7119cdac9f1c7aeb39e8639
Alex Xu <ax90l0tl@gmail.com>
2023-01-17 02:15:53 -0800

    fixed library

--- CircuitPython/ros_motor_ctrl.py
+++ CircuitPython/ros_motor_ctrl.py
@@ -1,39 +1,14 @@
 import time
 import board
-import supervisor
-import pwmio
+import DRV8874_motor_driver
 
-# frequency = 500 is default
-motor1 = pwmio.PWMOut(board.D5, duty_cycle=0, frequency=500)
-motor2 = pwmio.PWMOut(board.D6, duty_cycle=0, frequency=500)
+motor = DRV8874_motor_driver.motor_pwm(board.D5, board.D6)
 
+def message_received(msg):
+    print("Received message:", msg.data)    
 
-def motor_run(speed, mode):
-    # Speed is from -1 to 1
-    # positive is CW
-    # negative is CCW
-    # PWM range is from 0 - 65535
-    on = 65535
-    off = 0
-    if mode == "coast":
-        if speed >= 0:
-            motor1.duty_cycle = int(speed * 65535)
-            motor2.duty_cycle = off
-        else:
-            motor1.duty_cycle = off
-            motor2.duty_cycle = int(-speed * 65535)
-    elif mode == "brake":
-        if speed >= 0:
-            motor2.duty_cycle = int(65535 - speed * 65535)
-            motor1.duty_cycle = on
-        elif speed < 0:
-            motor2.duty_cycle = on
-            motor1.duty_cycle = int(65535 + speed * 65535)
+rclpy.init(name="teensy_node")
+subscriber = rclpy.create_subscription(String, "Motor_ctrl", message_received)
+rclpy.spinOnce()
+time.sleep(1)
 
-
-while True:
-    if supervisor.runtime.serial_bytes_available:
-        value = input().strip()
-    print(f"Received: {value}\r")
-    motor_run(0, "coast")
-    time.sleep(0.01)
